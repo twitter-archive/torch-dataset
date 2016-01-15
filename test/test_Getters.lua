aa2f3b13d21f7b1808021be5bb0c28de9348f5f1
ztaylor <ztaylor@twitter.com>
2016-01-15 07:13:57 -0800

    Fixes #5.

--- test/test_Getters.lua
+++ test/test_Getters.lua
@@ -12,15 +12,13 @@ test {
       test.mustBeTrue(get(paths.concat(paths.dirname(paths.thisfile()), 'files/1.txt'), -1, 3) == nil, 'negative offset must return nil')
       test.mustBeTrue(get(paths.concat(paths.dirname(paths.thisfile()), 'files/1.txt'), 3, 13) == 'DEFGH', 'out of bounds length must return as much as possible')
    end,
-   --[[
    testHTTP = function()
       local get = Getters('http')
-      test.mustBeTrue(get('http://ton.smf1.twitter.com/ckoia_images/age/val/927872198') == nil, 'URLs with status code 404 must return nil')
-      test.mustBeTrue(get('http://ton.smf1.twitter.com/ckoia_images/image_net/256/5eb47b529c4e59af4b76602eb7097147.jpg') ~= nil, 'URLs with status code 200 must return non-nil')
-      test.mustBeTrue(get('http://toxnx.smf1.twitter.com/ckoia_images/image_net/256/5eb47b529c4e59af4b76602eb7097147.jpg') == nil, 'URLs with invalid host names must return nil')
-      test.mustBeTrue(get('http://ton.smf1.twitter.com/ckoia_images/image_net/256/5eb47b529c4e59af4b76602eb7097147.jpg', 6, 2) == 'JF', 'URLs with offset and length should work')
+      test.mustBeTrue(get('http://torch.data.s3.amazonaws.com/dataset/cifar10/2914591350') == nil, 'URLs with status code 404 must return nil')
+      test.mustBeTrue(get('http://torch.data.s3.amazonaws.com/dataset/cifar10/2914591350.png', 13, 2) == 'HD', 'URLs with offset and length should work')
+      test.mustBeTrue(get('http://torch.data.s3.amazonaws.com/dataset/cifar10/2914591350.png') ~= nil, 'URLs with status code 200 must return non-nil')
+      test.mustBeTrue(get('http://xtorch.data.s3.amazonaws.comx/dataset/cifar10/2914591350.png') == nil, 'URLs with invalid host names must return nil')
    end,
-   --]]
    testTensor = function()
       local get = Getters(nil, 'Tensor')
       local x = torch.randn(3, 3)
